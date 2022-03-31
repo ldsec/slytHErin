6067d2b4d429cc9c91cbbf3f09887e830e54ba13
intx <francesco.intoci@epfl.ch>
2022-03-31 20:19:53 +0200

    added addition operations for encrypted blocks

--- inference/cipherUtils/opsBlocks.go
+++ inference/cipherUtils/opsBlocks.go
@@ -116,5 +116,48 @@ func BlocksC2CMul(X *EncInput, W *EncWeightDiag, Box CkksBox) (C *EncInput, err 
 	return
 }
 
-//TO DO: for this define a plaintext input thingy
-//func AddBlocksC2C(X *EncInput, W *EncInput, Box)
+func AddBlocksC2C(A *EncInput, B *EncInput, Box CkksBox) (*EncInput, error) {
+	var err error
+	if A.RowP != B.RowP || A.ColP != B.ColP {
+		err = errors.New("Block partitions not compatible for addition")
+	}
+	if A.InnerRows != B.InnerRows || A.InnerCols != B.InnerCols {
+		err = errors.New("Inner dimensions not compatible for addition")
+	}
+	C := new(EncInput)
+	C.RowP = A.RowP
+	C.ColP = A.ColP
+	C.InnerRows = C.InnerRows
+	C.InnerCols = A.InnerCols
+	C.Blocks = make([][]*ckks.Ciphertext, C.RowP)
+	for i := range C.Blocks {
+		C.Blocks[i] = make([]*ckks.Ciphertext, C.ColP)
+		for j := range C.Blocks[0] {
+			C.Blocks[i][j] = Box.Evaluator.AddNew(A.Blocks[i][j], B.Blocks[i][j])
+		}
+	}
+	return C, err
+}
+
+func AddBlocksC2P(A *EncInput, B *PlainInput, Box CkksBox) (*EncInput, error) {
+	var err error
+	if A.RowP != B.RowP || A.ColP != B.ColP {
+		err = errors.New("Block partitions not compatible for addition")
+	}
+	if A.InnerRows != B.InnerRows || A.InnerCols != B.InnerCols {
+		err = errors.New("Inner dimensions not compatible for addition")
+	}
+	C := new(EncInput)
+	C.RowP = A.RowP
+	C.ColP = A.ColP
+	C.InnerRows = C.InnerRows
+	C.InnerCols = A.InnerCols
+	C.Blocks = make([][]*ckks.Ciphertext, C.RowP)
+	for i := range C.Blocks {
+		C.Blocks[i] = make([]*ckks.Ciphertext, C.ColP)
+		for j := range C.Blocks[0] {
+			C.Blocks[i][j] = Box.Evaluator.AddNew(A.Blocks[i][j], B.Blocks[i][j])
+		}
+	}
+	return C, err
+}
